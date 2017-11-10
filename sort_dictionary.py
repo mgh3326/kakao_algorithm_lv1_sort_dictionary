bb532986f366c0c443d7eee965d1dfe9921ce568
mgh3326 <mgh3326@office.kw.ac.kr>
2017-11-10 14:10:09 +0900

    error correction

--- sort_dictionary.py
+++ sort_dictionary.py
@@ -1,6 +1,6 @@
 def sort_dictionary(dic):
     '''입력받은 dic의 각 키와 값을 튜플로 만든 다음, 키 값을 기준으로 정렬해서 리스트에 넣으세요. 그 리스트를 return하면 됩니다.'''
-    for k, v, i in dic.items():
+    for k, v in dic.items():
         print("Key:", k, "||", "Value", v)
     return []
 
